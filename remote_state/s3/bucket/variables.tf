14b4ac5cc746b8dce32ef37e54ac72e40363f1e4
deobieta <deobieta@gmail.com>
2016-10-19 17:11:49 -0500

    delete default bucket name

--- remote_state/s3/bucket/variables.tf
+++ remote_state/s3/bucket/variables.tf
@@ -1,6 +1,5 @@
 variable "aws_user_bucket_creator" {}
 
 variable "terraform_state_bucket" {
-    default = "terraform.state.versioning"
     description = "S3 bucket name to store terraform state file"
 }

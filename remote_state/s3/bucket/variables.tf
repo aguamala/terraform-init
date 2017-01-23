7ed34b7c116dd8834c601c344bb9991de705c69d
deobieta <deobieta@gmail.com>
2017-01-23 15:04:41 -0600

    default bucket name

--- remote_state/s3/bucket/variables.tf
+++ remote_state/s3/bucket/variables.tf
@@ -2,5 +2,4 @@ variable "aws_user_bucket_creator" {}
 
 variable "terraform_state_bucket" {
     description = "S3 bucket name to store terraform state file"
-    default = "tf-state-files-bucket"
 }

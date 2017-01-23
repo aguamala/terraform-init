9cd91e07bcc0f020c8e9e3a33aadbdf4c7f0c07b
deobieta <deobieta@gmail.com>
2017-01-23 14:35:48 -0600

    variables description

--- aws/variables.tf
+++ aws/variables.tf
@@ -1,7 +1,7 @@
 
 
 variable "aws_terraform_profile" {
-    description = "Terraform AWS profile"
+    description = "Terraform AWS profile to be created"
 }
 variable "aws_terraform_region" {
     description = "AWS default region"

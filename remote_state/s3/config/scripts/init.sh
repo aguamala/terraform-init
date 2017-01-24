58bb66a2a93c7bfb3eef4d028bcb7a289618fb14
gabriel de obieta <deobieta@gmail.com>
2017-01-23 23:33:22 -0600

    fix init.sh

--- remote_state/s3/config/scripts/init.sh
+++ remote_state/s3/config/scripts/init.sh
@@ -3,9 +3,9 @@
 set -e
 
 if [ "$1" != "" ]; then
-  if [ ! -d  ".$1" ]; then
+  if [ ! -d  "$1" ]; then
     echo '==> initialize tfstate file for' $1
-    mkdir -p ".$1"
+    mkdir -p "$1"
   fi
 else
   echo '==> initialize root tfstate file'

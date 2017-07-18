9f3ad19c90e39f8b207ea48047e127b3cbfffcef
deobieta <deobieta@gmail.com>
2017-07-18 09:44:38 -0500

    s3 and vpc templates

---
+++ aws/storage/s3/module_backend_config_S3.tf
@@ -0,0 +1,19 @@
+#--------------------------------------------------------------
+# config  tfstate file backend
+#--------------------------------------------------------------
+
+module "S3_backend_config" {
+    source = "/home/gabo/github.com/aguamala/terraform-init//modules/aws/backend/s3/config"
+
+    #tfstate_write_users  = []
+
+    #tfstate_write_groups = []
+
+    #tfstate_write_roles  = []
+
+    tfstate_aws_profile = "${var.aws_terraform_profile}"
+    tfstate_bucket = "${data.terraform_remote_state.file.module_backend_s3_bucket_id}"
+    tfstate_aws_region = "${var.aws_terraform_region}"
+
+    tfstate_path = "storage/s3/"
+}
